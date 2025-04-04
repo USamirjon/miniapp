c2c62433b234854a5912c232d5e68904312535e1
Самирджон <102414700+USamirjon@users.noreply.github.com>
2025-04-04 13:56:08 +0300

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+  useEffect(() => {
+    if (window.Telegram && window.Telegram.WebApp) {
+      const tg = window.Telegram.WebApp;
+      console.log('Telegram initData:', tg.initData);
+      tg.ready();
+    }
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App container mt-5">
+        <div className="card text-center">
+          <div className="card-body">
+            <h1 className="card-title">Мой Telegram Mini App</h1>
+            <p className="card-text">Привет! Это интерфейс на Bootstrap.</p>
+            <button className="btn btn-primary">Нажми меня</button>
+          </div>
+        </div>
+      </div>
   );
 }
 
